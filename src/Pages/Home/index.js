9b617296bb92f54176665e59b8f50981c44752fe
Jonny Mo <moirjonathan@gmail.com>
2023-03-20 10:49:09 -0600

    Progress with animations with framer.

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAnimation, motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
 import { useLottie } from 'lottie-react';
 import Project1 from '../../Components/Project1';
 import Project2 from '../../Components/Project2';
@@ -15,6 +18,19 @@ import Profile from '../../Assets/profileMoir-removebg.png';
 import './index.css';
 
 export default function Home() {
+	const squareVariants = {
+		visible: { opacity: 1, scale: 4, transition: { duration: 1 } },
+		hidden: { opacity: 0, scale: 0 },
+	};
+
+	const controls = useAnimation();
+	const [ref, inView] = useInView();
+	useEffect(() => {
+		if (inView) {
+			controls.start('visible');
+		}
+	}, [controls, inView]);
+
 	const [showDetails, setShowDetails] = useState(0);
 
 	const style = {
@@ -27,41 +43,16 @@ export default function Home() {
 	};
 	const { View } = useLottie(options, style);
 
-	const distance = (1500 / (window.innerWidth * 100)) * 10000 + 'vw';
-	console.log('inner width', window.innerWidth);
-	console.log(distance);
-
-	const ScrollRight = () => {
-		const el = document.getElementById('container');
-		let x = (window.innerWidth * 77) / 100;
-		const y = 0;
-		console.log('x value:', x);
-		console.log('y value:', y);
-
-		el.style.scrollBehavior = 'smooth';
-		el.scrollLeft = x;
-	};
-
-	const ScrollLeft = () => {
-		const el = document.getElementById('container');
-		let x = -((window.innerWidth * 77) / 100);
-		const y = 0;
-		console.log('x value:', x);
-		console.log('y value:', y);
-
-		el.style.scrollBehavior = 'smooth';
-		el.scrollLeft = x;
-	};
-
 	return (
 		<div style={{ boxSizing: 'border-box' }}>
 			<div className="flex flex-row gap-40 justify-center items-center p-20 ">
 				<div className=" flex flex-col justify-center bg-slate-200 rounded-xl shadow-xl p-8 ">
-					<div className="text-6xl font-bold">JON MOIR</div>
+					<div className="font-bold">JON MOIR</div>
 					<div>{View}</div>
-					<div className="text-4xl font-bold p-5 drop-shadow-xl">
+					<div className=" font-bold p-5 drop-shadow-xl">
 						FRONT END DEVELOPER
 					</div>
+					<h3>development/design/deployment</h3>
 					<div>Always learning through curiosity and imagination</div>
 					<div>
 						I design and code clean and performant applications, and I love what
@@ -88,118 +79,64 @@ export default function Home() {
 				</div>
 			</div>
 
-			<div className="flex justify-center text-4xl border-4 p-4 m-4 rounded-2xl">
-				<div className="flex flex-row align-middle p-10">
-					<button
-						className="h-100 w-100 bg-yellow-300 p-10 m-4"
-						onClick={ScrollLeft}
-					>
-						{'<'}
-					</button>
-					<div
-						id="container"
-						className="scrollbar-hide6"
-						style={{
-							display: 'flex',
-							flexDirection: 'row',
-							height: '400px',
-							maxWidth: '77vw',
-							overflowX: 'hidden',
-							border: '1px solid black',
-						}}
-					>
-						<div
-							style={{
-								backgroundColor: 'yellow',
-								minWidth: `${distance}`,
-								margin: '10px 10px 10px 20px',
-							}}
-						>
-							Show 1
-						</div>
-						<div
-							style={{
-								backgroundColor: 'yellow',
-								minWidth: `${distance}`,
-								margin: '10px 10px 10px 20px',
-							}}
-						>
-							Show 2
-						</div>
-						<div
-							style={{
-								backgroundColor: 'yellow',
-								minWidth: `${distance}`,
-								margin: '10px 10px 10px 20px',
-							}}
-						>
-							Show 3
-						</div>
-						<div
-							style={{
-								backgroundColor: 'yellow',
-								minWidth: `${distance}`,
-								margin: '10px 10px 10px 20px',
-							}}
-						>
-							Show 4
-						</div>
-					</div>
-					<button
-						className="h-100 w-100 bg-yellow-300 p-10 m-4"
-						onClick={ScrollRight}
-					>
-						{'>'}
-					</button>
-				</div>
-			</div>
+			<div className="text-xs border-4 m-4 rounded-2xl">
+				<motion.div
+					ref={ref}
+					animate={controls}
+					initial="hidden"
+					variants={squareVariants}
+					className="mx-auto my-96 max-w-screen-md bg-slate-200 rounded-2xl shadow-2xl relative"
+				>
+					<div className="font-bold p-2">Skills</div>
 
-			<div className="text-xl border-4 p-4 m-4 rounded-2xl">
-				<h1>Skills</h1>
-				<h3>development/design/deployment</h3>
-
-				<ul>
-					<li>
-						Knowledgeable in React.js, Redux, Next.js, Express, Tailwind,
-						Bootstrap, Styled Components, Material UI, ChakraUI, and Ant Design
-					</li>
-					<li>
-						Understanding of Agile development methodologies and CI/CD pipelines
-					</li>
-					<li>
-						Experimental experience with Node.js, MongoDB, Firebase, Apollo
-						Client, and GraphQL for full-stack development
-					</li>
-					<li>
-						UI/UX design using Figma and Adobe XD for high and low-fidelity
-						prototypes
-					</li>
-					<li>
-						Deployment using Netlify, Vercel, AWS, Render, and applying custom
-						domains
-					</li>
-				</ul>
+					<ul className="text-left px-20 pb-6 list-disc">
+						<li>
+							Knowledgeable in React.js, Redux, Next.js, Express, Tailwind,
+							Bootstrap, Styled Components, Material UI, ChakraUI, and Ant
+							Design
+						</li>
+						<li>
+							Understanding of Agile development methodologies and CI/CD
+							pipelines
+						</li>
+						<li>
+							Experimental experience with Node.js, MongoDB, Firebase, Apollo
+							Client, and GraphQL for full-stack development
+						</li>
+						<li>
+							UI/UX design using Figma and Adobe XD for high and low-fidelity
+							prototypes
+						</li>
+						<li>
+							Deployment using Netlify, Vercel, AWS, Render, and applying custom
+							domains
+						</li>
+					</ul>
+				</motion.div>
 			</div>
 
 			<div className="border-4 m-4 rounded-2xl h-fit p-10">
-				<div className="text-2xl p-4">Projects</div>
-				<div>
-					<ul className="flex flex-wrap gap-20 justify-center p-8">
+				<div className=" p-4">Projects</div>
+				<div className="mx-40">
+					<ul className="flex flex-wrap gap-20">
 						<li>
 							<button
 								onClick={() => {
 									setShowDetails(1);
 								}}
 							>
-								<div className="flex flex-col justify-center h-fit w-auto bg-slate-200 rounded-xl p-4 shadow-xl hover:scale-125 hover:bg-slate-400">
-									<div className="text-xl font-bold">
+								<div
+									className="flex flex-col justify-center bg-slate-200 rounded-xl shadow-xl hover:scale-125 hover:bg-slate-400"
+									style={{ width: '20vw', minHeight: '15vh' }}
+								>
+									<div className="text-xl font-bold p-4">
 										MKTFY - Web Application
 									</div>
 
 									<img
 										src={MKTFY}
 										alt="MKTFY-log"
-										className="p-8 h-56 object-cover rounded-xl drop-shadow-xl"
+										className="p-8 object-cover rounded-xl drop-shadow-xl"
 									/>
 								</div>
 							</button>
@@ -216,15 +153,18 @@ export default function Home() {
 									setShowDetails(2);
 								}}
 							>
-								<div className="flex flex-col justify-center h-fit w-auto bg-slate-200 rounded-xl p-4 shadow-xl hover:scale-125 hover:bg-slate-400">
-									<div className="text-xl font-bold">
+								<div
+									className="flex flex-col justify-center bg-slate-200 rounded-xl shadow-xl hover:scale-125 hover:bg-slate-400"
+									style={{ width: '20vw', minHeight: '15vh' }}
+								>
+									<div className="text-xl font-bold p-4">
 										MKTFY - Marketing Site
 									</div>
 
 									<img
 										src={MKTFYalt}
 										alt="MKTFY-log"
-										className="drop-shadow-xl p-8 h-56 object-cover -z-1"
+										className="drop-shadow-xl p-8 object-cover -z-1"
 									/>
 								</div>
 							</button>
@@ -241,15 +181,18 @@ export default function Home() {
 									setShowDetails(3);
 								}}
 							>
-								<div className="flex flex-col justify-center h-fit w-auto bg-slate-200 rounded-xl p-4 shadow-xl hover:scale-125 hover:bg-slate-400">
-									<div className="text-xl font-bold">
+								<div
+									className="flex flex-col justify-center bg-slate-200 rounded-xl shadow-xl hover:scale-125 hover:bg-slate-400"
+									style={{ width: '20vw', minHeight: '15vh' }}
+								>
+									<div className="text-xl font-bold p-4">
 										Movie Search - MERN-Stack Project
 									</div>
 
 									<img
 										src={MERN}
 										alt="MERN project"
-										className="drop-shadow-xl p-8 h-56 object-cover"
+										className="drop-shadow-xl p-8 object-cover"
 									/>
 								</div>
 							</button>
@@ -266,14 +209,18 @@ export default function Home() {
 									setShowDetails(4);
 								}}
 							>
-								<div className="flex flex-col justify-center h-fit w-auto bg-slate-200 rounded-xl p-4 shadow-xl hover:scale-125 hover:bg-slate-400">
-									<div className="text-xl font-bold">
+								<div
+									className="flex flex-col justify-center bg-slate-200 rounded-xl shadow-xl hover:scale-125 hover:bg-slate-400 "
+									style={{ width: '20vw', minHeight: '15vh' }}
+								>
+									<div className="text-xl font-bold p-4">
 										Old Portfolio - See how far I've come!
 									</div>
+
 									<img
 										src={OG}
 										alt="MKTFY-log"
-										className="drop-shadow-xl p-8 h-56 object-cover"
+										className="drop-shadow-xl p-8 object-cover"
 									/>
 								</div>
 							</button>
@@ -287,45 +234,53 @@ export default function Home() {
 					</ul>
 				</div>
 			</div>
-			<div className="text-2xl border-4 p-4 m-4 rounded-2xl">
-				<div className="text-2xl font-bold">
-					Courses, Degrees, and Professional Experience
-				</div>
-				<div>
-					<div className="text-xl font-semibold ">Courses & Degrees</div>
-					<ul>
-						<li>Front End Developer Certificate, Launchpad by Vog (2023)</li>
-						<li>
-							Google UI/UX Professional Design Certificate, Coursera (2022)
-						</li>
-						<li>Responsive Design Certificate, Free Code Camp</li>
-						<li>B.Sc. Civil Engineering University of Alberta (2013)</li>
-						<li>B.Ed. Mathematics University of Calgary (2019)</li>
-					</ul>
-					<div className="text-xl font-semibold">Professional Experience</div>
-					<ul>
-						<li>
-							<div>
-								<h2>
-									Educator | 2019 - 2022 Calgary Board of Education | Calgary
-								</h2>
-								Designed and created virtual classroom teaching methodologies
-								for middle school Math & Science students during the pandemic.
-								Built meaningful relationships with and fostered a positive
-								learning environment.
-							</div>
-						</li>
-						<li>
-							<h2>Civil Designer | 2015 - 2018 EXP inc. | Calgary</h2>
-						</li>
-						Provided drafting and design services for complex land development
-						projects, implemented innovative practices and staff training, and
-						resolved construction issues with the site managers and field
-						supervisors.
-					</ul>
-				</div>
+			<div className="text-xs border-4 m-4 rounded-2xl p-20">
+				<motion.div
+					ref={ref}
+					animate={controls}
+					initial="hidden"
+					variants={squareVariants}
+					className="mx-auto my-96 max-w-screen-lg bg-slate-200 rounded-2xl shadow-2xl relative"
+				>
+					<div className="font-bold">
+						Courses, Degrees, and Professional Experience
+					</div>
+					<div>
+						<div className="font-semibold ">Courses & Degrees</div>
+						<ul className="text-left px-20 pb-6 list-disc">
+							<li>Front End Developer Certificate, Launchpad by Vog (2023)</li>
+							<li>
+								Google UI/UX Professional Design Certificate, Coursera (2022)
+							</li>
+							<li>Responsive Design Certificate, Free Code Camp</li>
+							<li>B.Sc. Civil Engineering University of Alberta (2013)</li>
+							<li>B.Ed. Mathematics University of Calgary (2019)</li>
+						</ul>
+						<div className="font-semibold">Professional Experience</div>
+						<ul className="text-left px-20 pb-6 list-disc">
+							<li>
+								<div>
+									<h2>
+										Educator | 2019 - 2022 Calgary Board of Education | Calgary
+									</h2>
+									Designed and created virtual classroom teaching methodologies
+									for middle school Math & Science students during the pandemic.
+									Built meaningful relationships with and fostered a positive
+									learning environment.
+								</div>
+							</li>
+							<li>
+								<h2>Civil Designer | 2015 - 2018 EXP inc. | Calgary</h2>
+							</li>
+							Provided drafting and design services for complex land development
+							projects, implemented innovative practices and staff training, and
+							resolved construction issues with the site managers and field
+							supervisors.
+						</ul>
+					</div>
+				</motion.div>
 			</div>
-			<div className="text-2xl border-4 p-4 m-4 rounded-2xl">
+			<div className="border-4 p-4 m-4 rounded-2xl h-[900]">
 				<button
 					onClick={() => {
 						setShowDetails(5);
